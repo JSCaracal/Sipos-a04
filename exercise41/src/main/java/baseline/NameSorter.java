e77aa6d7159d86aae06f6a48870427073e3d337d
JSCaracal <siposj@knights.ucf.edu>
2021-10-12 19:05:25 -0400

    Adds skeleton code to the repo

---
+++ exercise41/src/main/java/baseline/NameSorter.java
@@ -0,0 +1,24 @@
+package baseline;
+
+import java.util.ArrayList;
+
+public class NameSorter {
+    //Declare Name List
+    private ArrayList<String> Names;
+
+
+    //Read the File and return the Name List
+    ArrayList<String> readNames(String fileName){
+        return;
+    }
+
+    //Sort the names in the List
+    void sortNames(){
+
+    }
+
+    //Write the Names to a file
+    void writeNames(String fileName){
+
+    }
+}
