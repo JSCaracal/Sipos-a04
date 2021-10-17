9e10609e2410d6eb3f0883de7ca7dea4843af0ef
JSCaracal <siposj@knights.ucf.edu>
2021-10-17 11:25:41 -0400

    Adds main program, test cases to be added

--- exercise41/src/main/java/baseline/NameSorter.java
+++ exercise41/src/main/java/baseline/NameSorter.java
@@ -1,8 +1,8 @@
 package baseline;
 
-import java.io.File;
-import java.io.FileNotFoundException;
+import java.io.*;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Scanner;
 
 public class NameSorter {
@@ -30,15 +30,39 @@ public class NameSorter {
     //Sort the names in the List
     void sortNames(){
 
+        Collections.sort(this.Names);
     }
 
     void printNames(){
-        System.out.println(Names.get(0));
+        System.out.printf("Total of %d names\n-----------------\n",this.Names.size());
+        for (String name:Names) {
+            System.out.println(name);
+        }
+        System.out.println();
     }
 
 
     //Write the Names to a file
     void writeNames(String fileName){
 
+        try{
+            File output = new File(fileName);
+            if(output.createNewFile()){
+                System.out.println("File has been created");
+            }
+        }catch (IOException e){
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
+        try {
+            FileWriter writer = new FileWriter(fileName);
+            writer.write("The number of people are "+this.Names.size()+"\n-----------------\n");
+            for (String name:this.Names) {
+                writer.write(name+"\n");
+            }
+            writer.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
