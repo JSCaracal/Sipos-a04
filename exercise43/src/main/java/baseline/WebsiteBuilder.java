2deb524776422aee15e4e595b9a01eaa3fd915ac
JSCaracal <siposj@knights.ucf.edu>
2021-10-17 19:18:45 -0400

    Adds psuedo code

---
+++ exercise43/src/main/java/baseline/WebsiteBuilder.java
@@ -0,0 +1,27 @@
+package baseline;
+
+public class WebsiteBuilder {
+    String authorName;
+    String websiteName;
+    //Ask the user for the website name and their author name
+    void askForCredentials(){
+
+    }
+
+    //Asks for JS Folder, if yes, creates it using Folder creator
+    void askForFolderJS(){
+
+    }
+    //Asks for CSS Folder, if yes, creates it using Folder creator
+    void askForCSS(){
+
+    }
+    //Creates the directory
+    void FolderCreator(String path){
+
+    }
+    //Creates the HTML Document
+    void HTMLCreation(){
+        
+    }
+}
