8eea3e19307ff6efac4e340f976d1d421d10d976
JSCaracal <siposj@knights.ucf.edu>
2021-10-17 20:33:53 -0400

    fixes directory issues

--- exercise41/src/main/java/baseline/Solution41.java
+++ exercise41/src/main/java/baseline/Solution41.java
@@ -5,10 +5,10 @@ import java.io.File;
 public class Solution41 {
     public static void main(String[] args) {
         NameSorter nameList = new NameSorter();
-        nameList.readNames("C:\\Users\\Joshua\\Documents\\COP3330\\Sipos-a04\\exercise41\\data\\exercise41_input.txt");
+        nameList.readNames(".\\data\\exercise41_input.txt");
         nameList.sortNames();
         nameList.printNames();
-        nameList.writeNames("C:\\Users\\Joshua\\Documents\\COP3330\\Sipos-a04\\exercise41\\data\\exercise41_output.txt");
+        nameList.writeNames(".\\data\\exercise41_output.txt");
 
     }
 }
