b259ce35d305e5cb542bc3a72dc655bfeae88a91
JSCaracal <siposj@knights.ucf.edu>
2021-10-17 20:31:48 -0400

    fixes issue with directory

--- exercise42/src/main/java/baseline/Solution42.java
+++ exercise42/src/main/java/baseline/Solution42.java
@@ -3,8 +3,8 @@ package baseline;
 public class Solution42 {
     public static void main(String[] args) {
         DataParsing parser = new DataParsing();
-        parser.readData("C:\\Users\\Joshua\\Documents\\COP3330\\Sipos-a04\\exercise42\\data\\exercise42_input.txt");
+        parser.readData(".\\data\\exercise42_input.txt");
         parser.printData();
-        parser.writeData("C:\\Users\\Joshua\\Documents\\COP3330\\Sipos-a04\\exercise42\\data\\exercise42_output.txt");
+        parser.writeData(".\\data\\exercise42_output.txt");
     }
 }
